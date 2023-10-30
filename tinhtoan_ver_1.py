c3f46e647a5b047e8a2502e2deb93b00ad761572
khoa học dữ liệu 17A2 <ntdung.dhkl17a2hn@sv.uneti.edu.vn>
2023-10-30 18:41:19 +0700

    Thêm file baitap

---
+++ tinhtoan_ver_1.py
@@ -0,0 +1,12 @@
+#Chương trình thứ hai
+#Tính toán số học 
+x=10
+y=5
+tong=x+y
+hieu=5
+tich=x*y
+thuong=x/y
+print('Tổng của hai số ', x, '+', y,'=',tong)
+print('Hiệu của hai số ', x, '-', y,'=',hieu)
+print('Tích của hai số ', x, '*', y,'=',tich)
+print('Thương của hai số ', x, '/',y, '=',thuong)
